59e8cbd0cf3411134af54dc90a8be7a3e5425eb8
Harris Naeem <40406646+harrisMLEng@users.noreply.github.com>
2026-08-06 17:28:19 +0100

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.py
@@ -0,0 +1,35 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # 2,3,4,4,5,10,20
+
+        cache = set(nums)
+        longest = 0
+        for num in nums:
+            if num - 1 not in cache:
+                count = 1
+                while num in cache:
+                    if num+1 in cache:
+                        cache.remove(num)
+                        num+=1
+                        count+=1
+                    else:
+                        cache.remove(num)
+                longest=max(longest,count)
+            
+        
+        
+        
+        # cache = set(nums)
+        # longest = 0
+        # for num in nums:
+        #     if num - 1 not in cache:
+        #         count = 1
+        #         while num in cache:
+        #             if num + 1 in cache:
+        #                 cache.remove(num)
+        #                 count+=1
+        #                 num +=1
+        #             else:
+        #                 cache.remove(num)
+        #         longest = max(longest, count)
+        return longest
